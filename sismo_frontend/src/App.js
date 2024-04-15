4a4ba85a583414f94a6232182b7a8e947aeacb06
romi_escobar <romi.escobar1615@gmail.com>
2024-04-15 10:20:02 -0400

    more attributes are shown

--- sismo_frontend/src/App.js
+++ sismo_frontend/src/App.js
@@ -23,7 +23,7 @@ const FeatureList = () => {
           };
         }
 
-        const response = await axios.get(`http://127.0.0.1:3000/api/v1/features`, {
+        const response = await axios.get(`http://127.0.0.1:3000/api/features`, {
           params: params
         });
         setFeatures(response.data.data);
@@ -52,7 +52,7 @@ const FeatureList = () => {
 
   const handleCommentSubmit = async (featureId, comment) => {
     try {
-      const response = await axios.post(`http://127.0.0.1:3000/api/v1/features/${featureId}/comments`, {
+      const response = await axios.post(`http://127.0.0.1:3000/api/features/${featureId}/comments`, {
         comment: { body: comment }
       });
       // Actualizar los comentarios solo si se agregó con éxito
@@ -69,7 +69,7 @@ const FeatureList = () => {
 
   const fetchComments = async (featureId) => {
     try {
-      const response = await axios.get(`http://127.0.0.1:3000/api/v1/features/${featureId}/comments`);
+      const response = await axios.get(`http://127.0.0.1:3000/api/features/${featureId}/comments`);
       setComments(prevComments => ({
         ...prevComments,
         [featureId]: response.data
@@ -88,7 +88,7 @@ const FeatureList = () => {
 
   return (
     <div>
-      <h1>Lista de Features</h1>
+      <h1>Lista de Eventos Sísmicos</h1>
       <div>
         <label htmlFor="perPage">Features por página:</label>
         <select id="perPage" value={perPage} onChange={handlePerPageChange}>
@@ -117,8 +117,13 @@ const FeatureList = () => {
         {features.map(feature => (
           <li key={feature.id}>
             <p>Titulo: {feature.title}</p>
+            <p>Fecha: {feature.time}</p>
             <p>Magnitud: {feature.magnitude}</p>
+            <p>Tipo: {feature.mag_type}</p>
             <p>Lugar: {feature.place}</p>
+            <p>Latitud: {feature.latitude}</p>
+            <p>Longitud: {feature.longitude}</p>
+            <p>Tsunami: {feature.tsunami ? 'sí' : 'no'}</p>
             {/* Agregar campo para comentarios */}
             <div>
               <input type="text" placeholder="Agregar comentario..." onKeyDown={(e) => {
@@ -140,6 +145,19 @@ const FeatureList = () => {
         <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>Anterior</button>
         <span>Página {currentPage} de {totalPages}</span>
         <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>Siguiente</button>
+      </div>
+      <div>
+        <h2>Tipos de Magnitudes</h2>
+        <ul>
+          <li><strong>Md: Magnitud de duración</strong>. Es una medida de la duración total de las ondas sísmicas.</li>
+          <li><strong>Ml: Magnitud de onda local</strong>. Es una medida de la amplitud máxima de las ondas sísmicas registradas en una estación de monitoreo.</li>
+          <li><strong>Ms: Magnitud superficial</strong>. Es una medida de la amplitud de las ondas sísmicas registradas en la superficie de la Tierra.</li>
+          <li><strong>Mw: Magnitud de momento</strong>. Es una medida de la cantidad total de energía liberada por un terremoto, calculada a partir de la cantidad de desplazamiento a lo largo de la falla y la rigidez de las rocas.</li>
+          <li><strong>Me: Magnitud de energía</strong>. Es una medida de la cantidad de energía liberada por un terremoto.</li>
+          <li><strong>Mi: Magnitud de intensidad</strong>. Es una medida de los efectos del terremoto en la superficie de la Tierra y en las estructuras humanas.</li>
+          <li><strong>Mb: Magnitud de onda corporal</strong>. Es una medida de la amplitud de las ondas sísmicas registradas en el interior de la Tierra.</li>
+          <li><strong>Mlg: Magnitud de onda local (variante)</strong>. Similar a Ml, pero ajustada para distancias más grandes.</li>
+        </ul>
       </div>
     </div>
   );
